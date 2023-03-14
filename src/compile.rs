03683e802bc7b1887e7b4f6c99388797f5a8d680
Johan-Mi <johanmi@protonmail.com>
2023-03-14 15:18:58 +0100

    Parse the source code

--- src/compile.rs
+++ src/compile.rs
@@ -1,5 +1,13 @@
 use std::path::Path;
 
 pub fn compile(source_file: &Path) {
+    let source_code = std::fs::read_to_string(source_file).unwrap();
+
+    let language = tree_sitter_gneiss::language();
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(language).unwrap();
+    let tree = parser.parse(&source_code, None).unwrap();
+    println!("{}", tree.root_node().to_sexp());
+
     todo!();
 }
